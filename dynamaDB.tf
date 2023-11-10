67b3c9cb07cd963811f8881fc05cd9a67158b7f8
Havoc13naveen <naveenkumar974393@gmail.com>
2023-11-10 11:19:06 +0530

    deleted

--- dynamaDB.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_dynamodb_table" "dydb" {
-    hash_key = "LockID"
-    name = "backend_dydb"
-    billing_mode = "PAY_PER_REQUEST"
-    attribute {
-      name = "LockID"
-      type = "S"
-    }
-}
--- ec2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "my_ec2"{
-    ami= "ami-0a7cf821b91bcccbc"
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.public_subnet.id
-    key_name = aws_key_pair.newkeypair.key_name
-    tags = {
-        Name = "ubuntu"
-    }
-}
